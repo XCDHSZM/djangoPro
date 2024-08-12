07a56ba8f2060f48fd85d3da9ae749128abb2aaa
shenzhiming <3108813554@qq.com>
2024-08-12 23:16:13 +0800

    feat:完成新的布局结构

--- myback/Data/static/js/index.js
+++ myback/Data/static/js/index.js
@@ -44,17 +44,4 @@ $(function () {
         $(".sidebar-link").removeClass("is-active");
         $(".trending").addClass("is-active");
     });
-
-    $(".video").click(function () {
-        var source = $(this).find("source").attr("src");
-        var title = $(this).find(".video-name").text();
-        var person = $(this).find(".video-by").text();
-        var img = $(this).find(".author-img").attr("src");
-        $(".video-stream video").stop();
-        $(".video-stream source").attr("src", source);
-        $(".video-stream video").load();
-        $(".video-p-title").text(title);
-        $(".video-p-name").text(person);
-        $(".video-detail .author-img").attr("src", img);
-    });
 });
